c3f006836adf3b2a164e3a9d8b24f22d34fcd736
LawrenceGlenn <Lawrence.O.Glenn@gmail.com>
2019-03-12 14:31:28 -0700

    corrected shimmer and consolidated changes

--- Sarah_Purse/Sarah_Purse.ino
+++ Sarah_Purse/Sarah_Purse.ino
@@ -5,55 +5,40 @@
 
 #define PIN 6
 
-#define NUM_LEDS 60
+#define NUM_LEDS 50
 
 #define BRIGHTNESS 200
 
-int colorInputPin = A0;
-int tintInputPin = A1;
-int brightnessInputPin = A2;
+#define colorInputPin A0
+
+#define tintInputPin A1
+
+#define brightnessInputPin A2
+
+#define inputMax 1023
+
+#define wait 10
+
 int colorInputValue = 0;
 int tintInputValue = 0;
 int brightnessInputValue = 0;
-int redValue = 0;
-int greenValue = 0;
-int blueValue = 0;
-int whiteValue = 0;
-int inputMax = 1024;
 int brightnessValue = 0;
-int wait = 10;
+
+int colorValue[4];
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRBW + NEO_KHZ800);
 
-
-int colorMatrix[NUM_LEDS][4];
-float currentColorMatrix[NUM_LEDS][5];
-float endColorMatrix[NUM_LEDS][5];
-
-
-byte neopix_gamma[] = {
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
-    1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
-    2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
-    5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
-   10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
-   17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
-   25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
-   37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
-   51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
-   69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
-   90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
-  115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
-  144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
-  177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
-  215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
-
+int startColorMatrix[NUM_LEDS][4];
+int endColorMatrix[NUM_LEDS][4];
+int currentColorMatrix[NUM_LEDS][4];
+float startMillis[NUM_LEDS];
+float endMillis[NUM_LEDS];
 
 void setup() {
   strip.setBrightness(BRIGHTNESS);
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
+  Serial.begin(9600);
   pinMode(colorInputPin, INPUT);
   pinMode(tintInputPin, INPUT);
   pinMode(brightnessInputPin, INPUT);
@@ -61,16 +46,18 @@ void setup() {
 
 void loop() {
   // read the value from the input:
-  setColorWheelInput();
-  setTintWheelInput();
-  setBrightnessWheelInput();
-
+ // setColorWheelInput();
+ // setTintWheelInput();
+ // setBrightnessWheelInput();
+ colorValue[0]=150;
+ colorValue[2]=100;
+shimmer();
   //loop through the pixels and set thier values
-      for(uint16_t i=0; i<strip.numPixels(); i++) {
-          strip.setPixelColor(i, strip.Color(redValue,greenValue,blueValue,whiteValue));
-        }
-        strip.setBrightness(brightnessValue);
-        strip.show();
+//      for(uint16_t i=0; i<strip.numPixels(); i++) {
+//          strip.setPixelColor(i, strip.Color(redValue,greenValue,blueValue,whiteValue));
+//        }
+//        strip.setBrightness(brightnessValue);
+//        strip.show();
       
         delay(wait);
 
@@ -90,29 +77,29 @@ void setColorWheelInput(){
  // decrease blue     1280  - 1535
   int x = map(colorInputValue, 0, inputMax, 0, 1535);
   if (x<=255) {
-    redValue = 255;
-    greenValue = x;
-    blueValue = 0;
+    colorValue[0] = 255;
+    colorValue[1] = x;
+    colorValue[2] = 0;
   }else if (x>=256 && x<=511){
-    redValue = 511-x;
-    greenValue = 255;
-    blueValue = 0;
+    colorValue[0] = 511-x;
+    colorValue[1] = 255;
+    colorValue[2] = 0;
   }else if (x>=512 && x<=767){
-    redValue = 0;
-    greenValue = 255;
-    blueValue = x-512;
+    colorValue[0] = 0;
+    colorValue[1] = 255;
+    colorValue[2] = x-512;
   }else if (x>=768 && x<=1023){
-    redValue = 0;
-    greenValue = 1023-x;
-    blueValue = 255;
+    colorValue[0] = 0;
+    colorValue[1] = 1023-x;
+    colorValue[2] = 255;
   }else if (x>=1024 && x<=1279){
-    redValue = x-1024;
-    greenValue = 0;
-    blueValue = 255;
+    colorValue[0] = x-1024;
+    colorValue[1] = 0;
+    colorValue[2] = 255;
   }else if (x>=1280 && x<=1536){
-    redValue = 255;
-    greenValue = 0;
-    blueValue = 1536-x;
+    colorValue[0] = 255;
+    colorValue[1] = 0;
+    colorValue[2] = 1536-x;
   }
   
 }
@@ -121,53 +108,34 @@ void setColorWheelInput(){
 void setTintWheelInput(){
   tintInputValue = analogRead(tintInputPin);
 
- // start at brightness and white off
- // increase brightness                         0     - 255
- // decrease brightness and increase white      256   - 511
-
-  whiteValue = map(tintInputValue, 0, inputMax, 0, 511);
+  colorValue[3] = map(tintInputValue, 0, inputMax, 0, 255);
   
 }
 
 void setBrightnessWheelInput(){
   brightnessInputValue = analogRead(brightnessInputPin);
   
-  brightnessValue = neopix_gamma[map(brightnessInputValue, 0, inputMax, 0, 255)];
+  brightnessValue = map(brightnessInputValue, 0, inputMax, 0, 255);
 }
 
 
 void shimmer(){
-
   for(int k=0; k<1;k++){
-    if(currentColorMatrix[k][4] == 0){
-      Serial.println("current");
-      Serial.println(currentColorMatrix[k][4]);
-      Serial.println("end time");
-      Serial.println(endColorMatrix[k][4]);
-      Serial.println("now");
-      Serial.println(millis());
-      currentColorMatrix[k][4] = millis();
-      Serial.println("TEST1");
-      Serial.println(currentColorMatrix[k][4]);
-      endColorMatrix[k][4] = currentColorMatrix[k][4]+5000;
-      endColorMatrix[k][0] = 255;
-      endColorMatrix[k][1] = 0;
-      Serial.println("TEST1");
-      Serial.println(currentColorMatrix[k][4]);
-      endColorMatrix[k][2] = 0;
-      endColorMatrix[k][3] = 0;
-    }
-      Serial.println("TEST2");
-      Serial.println(currentColorMatrix[k][4]);
-    currentColorMatrix[k][0] = map(millis(), currentColorMatrix[k][4], endColorMatrix[k][4], currentColorMatrix[k][0], endColorMatrix[k][0]);
-    currentColorMatrix[k][1] = map(millis(), currentColorMatrix[k][4], endColorMatrix[k][4], currentColorMatrix[k][1], endColorMatrix[k][1]);
-    currentColorMatrix[k][2] = map(millis(), currentColorMatrix[k][4], endColorMatrix[k][4], currentColorMatrix[k][2], endColorMatrix[k][2]);
-    currentColorMatrix[k][3] = map(millis(), currentColorMatrix[k][4], endColorMatrix[k][4], currentColorMatrix[k][3], endColorMatrix[k][3]);
-  
-      Serial.println("TEST3");
-      Serial.println(currentColorMatrix[k][4]);
+    if(startMillis[k] == 0){
+      for(int j=0; j<strip.numPixels();j++){
+        startColorMatrix[k][j] = currentColorMatrix[k][j];
+        endColorMatrix[k][j] = colorValue[j]-20+random(40);
       }
+      startMillis[k] = millis();
+      endMillis[k] = startMillis[k]+5000;
+    }
 
+    for(int j=0; j<strip.numPixels();j++){
+      if(currentColorMatrix[k][j] != endColorMatrix[k][j]){
+        currentColorMatrix[k][j] = map(millis(), startMillis[k], endMillis[k], startColorMatrix[k][j], endColorMatrix[k][j]);
+      }
+    }
+  }
   
 }
 // Input a value 0 to 255 to get a color value.
@@ -194,4 +162,3 @@ uint8_t green(uint32_t c) {
 uint8_t blue(uint32_t c) {
   return (c);
 }
-
